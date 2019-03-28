36e48699b8236b998302e1c68f7f132b6eabb11e
javargas1098 <ja.vargas1098@gmail.com>
2019-03-28 12:19:49 -0500

    finish corecion de paquete

--- target/classes/static/js/apiclient.js
+++ target/classes/static/js/apiclient.js
@@ -1,16 +1,29 @@
-apiclient = (function() {
-	return {
-		getWeather : function(city, callback) {		
-			$.get("wheather/" + city, function(data) {
-				callback(data);
-			});
 
-		},
-		error : function() {
-//			$('body').showMessage({thisMessage: ['Hey! Something bit me!']});
-			alert("There was an error. Try again please!");
-		}
+apiclient = (function() {
+
+	return {
+
+	getWeather :function (city) {
+	    axios.get('/wheather/' + city).then(function (response) {
+	        var texto = document.getElementById("clima");
+	        texto.innerHTML = "";
+	        for (datos in response.data) {
+	            if (datos == "main") {
+	                for (datos1 in response.data[datos]) {
+	                	texto = document.getElementById("clima");
+	                    var fila = document.createElement("tr");
+	                    fila.setAttribute("class", "lead text-muted");
+	                    fila.innerHTML = datos1 + ": " + response.data[datos][datos1];
+	                    texto.appendChild(fila);
+	                    console.log(response.data[datos][datos1]);
+	                }
+	            }
+	        }
+	    }).catch(function (error) {
+	        console.log(error);
+	        alert("Ciudad no encotrada");
+	    });
+	}
 	}
 
 })();
-
