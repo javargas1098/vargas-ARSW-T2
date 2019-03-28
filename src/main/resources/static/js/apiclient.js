391a67c445ff921ace9def1c18131f0776f833cf
javargas1098 <43441798+javargas1098@users.noreply.github.com>
2019-03-28 12:58:23 -0500

    Update apiclient.js

--- src/main/resources/static/js/apiclient.js
+++ src/main/resources/static/js/apiclient.js
@@ -1,29 +1,21 @@
-
-apiclient = (function() {
-
-	return {
-
-	getWeather :function (city) {
-	    axios.get('/wheather/' + city).then(function (response) {
-	        var texto = document.getElementById("clima");
-	        texto.innerHTML = "";
-	        for (datos in response.data) {
-	            if (datos == "main") {
-	                for (datos1 in response.data[datos]) {
-	                	texto = document.getElementById("clima");
-	                    var fila = document.createElement("tr");
-	                    fila.setAttribute("class", "lead text-muted");
-	                    fila.innerHTML = datos1 + ": " + response.data[datos][datos1];
-	                    texto.appendChild(fila);
-	                    console.log(response.data[datos][datos1]);
-	                }
-	            }
-	        }
-	    }).catch(function (error) {
-	        console.log(error);
-	        alert("error");
-	    });
-	}
-	}
-
-})();
+function getWeather(city) {
+    axios.get('/clima/' + city).then(function (response) {
+        var par = document.getElementById("Data");
+        par.innerHTML = "";
+        for (c in response.data) {
+            if (c == "main") {
+                for (m in response.data[c]) {
+                    par = document.getElementById("Data");
+                    var fila = document.createElement("P");
+                    fila.setAttribute("class", "lead text-muted");
+                    fila.innerHTML = m + ": " + response.data[c][m];
+                    par.appendChild(fila);
+                    console.log(response.data[c][m]);
+                }
+            }
+        }
+    }).catch(function (error) {
+        console.log(error);
+        alert("Ciudad no valida");
+    });
+}
